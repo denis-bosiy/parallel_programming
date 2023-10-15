1c83fc33667f0437ad106d57ceab9dd36ed3e3fa
denis.bosyi <denis.bosiy@gmail.com>
2023-10-15 19:59:48 +0300

    Исправлен код, выявленный в замечаниях, по 1-й лабе

--- lw_1/lw_1.cpp
+++ lw_1/lw_1.cpp
@@ -10,21 +10,21 @@ DWORD WINAPI ThreadProc(CONST LPVOID lpParam)
 	const int threadIndex = *static_cast<int*>(lpParam);
 	delete lpParam;
 
-	std::cout << "Thread number";
-	std::cout << threadIndex;
-	std::cout << " is executing" << std::endl;
+	std::cout << "Thread number" + std::to_string(threadIndex) + " is executing\n";
 
 	ExitThread(0);
 }
 
 int ProcessArgv(int argc, char* argv[])
 {
-	if (argc - 1 != 1) {
+	if (argc - 1 != 1)
+	{
 		throw std::length_error("There should be one argument showing number of threads");
 	}
 
 	const int threadsCount = std::stoi(argv[1]);
-	if (threadsCount < 1 || threadsCount > MAX_THREADS_COUNT) {
+	if (threadsCount < 1 || threadsCount > MAX_THREADS_COUNT)
+	{
 		const std::string errorMsg = "Threads count should be less than " +
 			std::to_string(MAX_THREADS_COUNT + 1) +
 			" and more than 0";
@@ -40,7 +40,8 @@ int main(int argc, char* argv[])
 	{
 		const int threadsCount = ProcessArgv(argc, argv);
 		HANDLE* handles = new HANDLE[threadsCount];
-		for (int i = 0; i < threadsCount; i++) {
+		for (int i = 0; i < threadsCount; i++)
+		{
 			int* threadIndex = new int(i);
 			handles[i] = CreateThread(NULL, 0, &ThreadProc, threadIndex, CREATE_SUSPENDED, NULL);
 		}
